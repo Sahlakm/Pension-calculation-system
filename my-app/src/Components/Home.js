d1188b8500a67102fe376f0840d17f5feafb279f
Likhil115 <likhilvenkat123@gmail.com>
2025-03-27 16:04:53 +0530

    addedbuttons

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -1,15 +1,18 @@
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 padding">
       <h1 className="text-3xl font-bold mb-6">Pension Management System</h1>
-      <button onClick={() => navigate("/add-employee")} className="p-3 bg-green-500 text-white rounded-lg">
-        Enter Employee Details
+      <div className="btns-container">
+      <button onClick={() => navigate("/add-employee")} className="p-3 bg-green-500 text-white rounded-lg btn-container">
+        Enter New Pensioner Details
       </button>
-      
+      <button className="p-3 bg-green-500 text-white rounded-lg btn-container" onClick={() => alert("Yet to be implemented ...")}> Update Tax / Deductions </button>
+      <button className="p-3 bg-green-500 text-white rounded-lg btn-container" onClick={() => alert("Yet to be implemented ...")}> Generate Reports </button></div>
     </div>
   );
 };
